b717f0cbd395389b8b7faf02cf1a0f95c5f8f3d5
saikatnoman12 <noommaa240@gmail.com>
2022-11-04 10:47:32 +0600

    add typeScript compilation process

---
+++ a. typeScript compilation/src/script.ts
@@ -0,0 +1,26 @@
+// const myName = 'Abdullah Al Nomaan';
+
+console.log('hello typescript');
+
+const myName = 'Abdullah Al Nomaan';
+console.log(myName);
+
+console.log('hello 1');
+console.log('hello 2');
+console.log('hello 3');
+console.log('hello 4');
+console.log('hello 5');
+console.log('hello 6');
+console.log('hello 7');
+console.log('hello 8');
+console.log('hello 9');
+
+console.log('hello 1');
+console.log('hello 2');
+console.log('hello 3');
+console.log('hello 4');
+console.log('hello 5');
+console.log('hello 6');
+console.log('hello 7');
+console.log('hello 8');
+console.log('hello 9');
